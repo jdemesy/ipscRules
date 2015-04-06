b6d5768c4d7eecbd934911241730142f3f839eb9
Julien DEMESY <julien.demesy@gmail.com>
2015-04-06 15:49:13 +0200

    Cleaned base app. Added search form.

--- www/js/app.js
+++ www/js/app.js
@@ -34,7 +34,8 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     url: "/search",
     views: {
       'menuContent': {
-        templateUrl: "templates/search.html"
+        templateUrl: "templates/search.html",
+        controller: 'SearchCtrl'
       }
     }
   })
@@ -46,26 +47,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         templateUrl: "templates/browse.html"
       }
     }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: "/playlists/:playlistId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
-      }
-    }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/search');
 });
